1e97c0acb3e3935f84de79e5e2aae882d151c215
Atharva Rakhunde <92033678+atharvarakhunde@users.noreply.github.com>
2024-10-31 19:07:02 +0530

    Update ExploreMenu.jsx

--- Frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ Frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -3,25 +3,32 @@ import React from 'react'
 import './ExploreMenu.css'
 import { menu_list } from '../../assets/frontend_assets/assets'
 // eslint-disable-next-line react/prop-types
-const ExploreMenu = ({category,setCategory}) => {
+const ExploreMenu = ({category, setCategory}) => {
   return (
     <div className='explore-menu' id='explore-menu'>
-        <h1>Explore our Menu</h1>
-        <p className="explore-menu-text">Satisfy your sweet tooth with our delicious cake collection! Find flavors that delight your taste buds and make every occasion special.</p> 
-        <div className="explore-menu-list">
-            {
-                menu_list.map((item,index)=>{
-                    return (
-                        <div onClick={()=>setCategory(prev=>prev===item.menu_name?"All":item.menu_name)} key={index} className="explore-menu-list-item">
-                            <img className={category===item.menu_name?"active":""} src={item.menu_image} alt="" />
-                            <p>{item.menu_name}</p>
-                        </div>
-                    )
-                })
-            }
-        </div>
+      <h1>Explore our Menu</h1>
+      <p className="explore-menu-text">Satisfy your sweet tooth with our delicious cake collection! Find flavors that delight your taste buds and make every occasion special.</p> 
+      <div className="explore-menu-list">
+        {
+          menu_list.map((item, index) => {
+            return (
+              <div  
+                onClick={() => {
+                  setCategory(prev => prev === item.menu_name ? "All" : item.menu_name);
+                  window.location.hash = "#food-display"; // Scroll to #food-display
+                }} 
+                key={index} 
+                className="explore-menu-list-item"
+              >
+                <img className={category === item.menu_name ? "active" : ""} src={item.menu_image} alt="" />
+                <p>{item.menu_name}</p>
+              </div>
+            )
+          })
+        }
+      </div>
     </div>
   )
 }
 
-export default ExploreMenu
+export default ExploreMenu;
